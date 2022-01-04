68dbd2a39ecaf74c99ef0169709faf6f781b9481
Ramesh <baralramesh07@gmail.com>
2022-01-04 18:27:48 +0545

    first commit

---
+++ src/main/java/com/example/agileintppmtool/AgileIntPpmToolApplication.java
@@ -0,0 +1,13 @@
+package com.example.agileintppmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgileIntPpmToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgileIntPpmToolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/agileintppmtool/AgileIntPpmToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.agileintppmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgileIntPpmToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
